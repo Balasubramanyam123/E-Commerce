aeb897df6e65a7d5a06055c61f4382d33e471237
unknown <Asus@DESKTOP-4H5S223>
2023-10-04 23:55:38 +0530

    first commit

--- user-authentication-app/src/main/java/org/niit/userauthenticationapp/service/JwtTokenGenerator.java
+++
@@ -1,9 +0,0 @@
-package org.niit.userauthenticationapp.service;
-
-import org.niit.userauthenticationapp.model.User;
-
-import java.util.Map;
-
-public interface JwtTokenGenerator {
-    public abstract Map<String,String> generateJwt(User user);
-}
--- vehicle-app/src/main/java/org/niit/vehicleapp/repository/UserProductRepository.java
+++
@@ -1,9 +0,0 @@
-package org.niit.vehicleapp.repository;
-
-import org.niit.vehicleapp.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserProductRepository extends MongoRepository<User,String> {
-}
